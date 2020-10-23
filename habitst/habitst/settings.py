21471ff95e725e17299b2d8819f014ca1950b784
LeeSeongSu <oplm123456@naver.com>
2020-10-23 20:58:24 +0900

    .

--- habitst/habitst/settings.py
+++ habitst/habitst/settings.py
@@ -235,7 +235,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('52.79.112.91', 8080)],
         },
     },
 }
